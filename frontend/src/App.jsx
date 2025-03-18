b18b9552673f823b86c5cb8f18ee94e6a7b1d746
prachi-dabhi <160569521+prachi-dabhi@users.noreply.github.com>
2025-03-18 08:35:19 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import { AuthProvider } from '../user-auth/src/Components/AuthProvider';
+import AdminLogin from '../user-auth/src/Components/UserLogin';
+import MainLogin from '../user-auth/src/Layouts/MainLogin';
+import UserLogin from '../user-auth/src/Components/CustomerRegistration';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { routingConstants } from '../user-auth/src/Constants/routingConstants';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const _router = [
+    {
+      path: routingConstants?.newLogin,
+      element: <UserLogin />
+    },
+    {
+      path: routingConstants?.userlogin,
+      element: <AdminLogin />
+    },
+    {
+      path: routingConstants?.mainLogin,
+      element: <MainLogin />
+    }
+  ]
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <AuthProvider> {/* Wrap everything with AuthProvider */}
+      <Router>
+        <Routes>
+          {_router.map((route, index) => (
+            <Route key={index} path={route.path} element={route.element} />
+          ))}
+        </Routes>
+      </Router>
+    </AuthProvider>
+  );
 }
 
-export default App
+export default App;
